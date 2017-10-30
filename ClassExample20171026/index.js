4aa9e5c2a64a0159dca2db7a3bdd96ce1234820d
Robert B. Houser <rb2017houser@gmail.com>
2017-10-30 11:50:19 -0400

    Added Robert Houser's version of NetFlix Binge to repository

--- ClassExample20171026/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Board from './board.js';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Board />, document.getElementById('app'));
-registerServiceWorker();
